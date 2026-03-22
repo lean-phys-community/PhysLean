6efab97f243017be83d8bff1a706fa078adb5641
Gabe <66077254+MrBrain295@users.noreply.github.com>
2026-03-22 08:36:00 -0500

    resore copyright

--- PhysLean/ClassicalMechanics/RigidBody/SolidSphere.lean
+++ PhysLean/ClassicalMechanics/RigidBody/SolidSphere.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Tooby-Smith
+-/
 module
 public import PhysLean.ClassicalMechanics.RigidBody.Basic
 public import PhysLean.ClassicalMechanics.RigidBody.SolidSphereHelper
