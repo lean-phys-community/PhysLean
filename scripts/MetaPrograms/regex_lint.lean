5a44aabd575380588a03462dd6510a149fb76102
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2026-04-05 07:37:00 +0100

    refactor: regex_lint
    Co-authored-by: Daniel Morrison <39346894+morrison-daniel@users.noreply.github.com>

--- scripts/MetaPrograms/regex_lint.lean
+++ scripts/MetaPrograms/regex_lint.lean
@@ -142,12 +142,13 @@ def main (_ : List String) : IO UInt32 := do
   unless (← mFile.pathExists) do
         throw <| IO.userError s!"object file '{mFile}' of module {imp.module} does not exist"
   let (hepLeanMod, _) ← readModuleData mFile
-  let filePaths := hepLeanMod.imports.filterMap (fun imp ↦
+let (physlibMod, _) ← readModuleData mFile
+let filePaths := physlibMod.imports.filterMap (fun imp ↦
     if imp.module == `Init then
       none
     else
       some ((mkFilePath (imp.module.toString.split (· == '.'))).addExtension "lean"))
-  let errors := (← filePaths.mapM hepLeanLintFile).flatten
+  let errors := (← filePaths.mapM physlibLintFile).flatten
   let errorMessagesPresent := (errors.map (fun e => e.error)).sortDedup
   for eM in errorMessagesPresent do
     IO.println s!"\n\nError: {eM}"
