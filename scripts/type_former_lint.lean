82afbf6381de55a863b42f6c74a54b99f13e3c53
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2026-04-05 07:53:16 +0100

    refactor: update type_former_lint

--- scripts/type_former_lint.lean
+++ scripts/type_former_lint.lean
@@ -18,9 +18,9 @@ in Physlib.
 open Lean System Meta
 
 
-/-- A rough definition of Upper Camal, checking that if a string starts with a
+/-- A rough definition of Upper Camel, checking that if a string starts with a
 latin letter then that letter is capital.  -/
-def IsUpperCamal (s : String) : Bool :=
+def IsUpperCamel (s : String) : Bool :=
   let parts := s.splitOn "."
   let lastPart := parts.get! (parts.length - 1)
   lastPart = "noConfusionType" ∨
@@ -34,17 +34,17 @@ def main (_ : List String) : IO UInt32 := do
   let mFile ← findOLean imp.module
   unless (← mFile.pathExists) do
         throw <| IO.userError s!"object file '{mFile}' of module {imp.module} does not exist"
-  let (hepLeanMod, _) ← readModuleData mFile
+  let (physlibMod, _) ← readModuleData mFile
   let mut warned := false
-  for imp in hepLeanMod.imports do
+  for imp in physlibMod.imports do
     let mFile ← findOLean imp.module
     let (modData, _) ← readModuleData mFile
     let cons := modData.constants
     for c in cons do
       if c.hasValue ∧ ¬ Lean.Name.isInternal c.name ∧ Lean.Compiler.LCNF.isTypeFormerType c.type
-        ∧ ¬ IsUpperCamal c.name.toString then
+        ∧ ¬ IsUpperCamel c.name.toString then
           warned := true
           IO.println s!"name: #{c.name}"
   if warned then
-    throw <| IO.userError s!"There are type former definitions not in upper camal style."
+    throw <| IO.userError s!"There are type former definitions not in upper camel style."
   pure 0
