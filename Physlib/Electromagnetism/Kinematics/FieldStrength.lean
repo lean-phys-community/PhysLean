4c1268b4ec4ecd1d055deb5ccd5d0b31df0eb324
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2026-05-01 09:09:17 +0100

    refactor: Add TODO item.

--- Physlib/Electromagnetism/Kinematics/FieldStrength.lean
+++ Physlib/Electromagnetism/Kinematics/FieldStrength.lean
@@ -240,6 +240,11 @@ TODO "Add a section in this file on the evaluation of the field strength tensor'
 
 -/
 
+TODO "For the electromagnetic field strength, we have lots of lemmas related
+  to the components of the field strength tensor in terms of the basis. For example,
+  `toTensor_toFieldStrength_basis_repr`, these should be removed. They are used
+  downstream, so there use there should be refactored."
+
 lemma toTensor_toFieldStrength_basis_repr {d} (A : ElectromagneticPotential d) (x : SpaceTime d)
     (b : ComponentIdx (S := realLorentzTensor d) (Fin.append ![Color.up] ![Color.up])) :
     (Tensor.basis _).repr (Tensorial.toTensor (toFieldStrength A x)) b =
