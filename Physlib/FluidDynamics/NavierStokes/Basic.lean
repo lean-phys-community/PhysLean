00c4ed9f6fc27efce9a13036d6fe02cfa7dcbf0c
Florian Wiesner <wiesnerflorian94@gmail.com>
2026-05-18 09:53:40 +0200

    feat: Add initial documentation for Navier-Stokes equations

---
+++ Physlib/FluidDynamics/NavierStokes/Basic.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2026 Florian Wiesner. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Florian Wiesner
+-/
+
+module
+
+
+/-!
+
+# The Navier-Stokes equations
+
+## i. Overview
+
+The Navier-Stokes equations are a set of partial differential equations that describe
+the motion of viscous fluid substances. They are fundamental in fluid dynamics and are
+used to model the behavior of fluids in various contexts, including gas flow and water flow.
+
+-/
