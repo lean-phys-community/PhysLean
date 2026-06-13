1aee78771aa2f628c2d60b242ba0e92981b439f9
Yanting Teng <adateng07@gmail.com>
2026-06-14 00:54:28 +0200

    followed TODO for redining FiniteHilbertSpace; modified TightBindingChain/Basic.lean to be compatible with the new definition.

--- Physlib/QuantumMechanics/FiniteTarget/HilbertSpace.lean
+++ Physlib/QuantumMechanics/FiniteTarget/HilbertSpace.lean
@@ -34,21 +34,121 @@ TODO "To match this with the results currently in the `QuantumInfo` part of the 
   parts of `./ResourceTheory/FreeState`."
 
 /-- The finite dimensional Hilbert space of dimension `n`. -/
-def FiniteHilbertSpace (n : ℕ) : Type := EuclideanSpace ℂ (Fin n)
+-- def FiniteHilbertSpace (n : ℕ) : Type := EuclideanSpace ℂ (Fin n)
 
-instance {n : ℕ} : AddCommGroup (FiniteHilbertSpace n) := inferInstanceAs
-  (AddCommGroup (EuclideanSpace ℂ (Fin n)))
+@[ext]
+structure FiniteHilbertSpace (d : Type*) [Fintype d] [DecidableEq d] where
+  /-- The underlying element of `EuclideanSpace ℂ d`. -/
+  val : EuclideanSpace ℂ d
 
-noncomputable instance {n : ℕ} : Module ℂ (FiniteHilbertSpace n) := inferInstanceAs
-  (Module ℂ (EuclideanSpace ℂ (Fin n)))
+@[inherit_doc FiniteHilbertSpace]
+scoped notation "𝓗[" d "]" => FiniteHilbertSpace d
 
-noncomputable instance {n : ℕ} : NormedAddCommGroup (FiniteHilbertSpace n) := inferInstanceAs
-  (NormedAddCommGroup (EuclideanSpace ℂ (Fin n)))
+-- instance {n : ℕ} : AddCommGroup (FiniteHilbertSpace n) := inferInstanceAs
+--   (AddCommGroup (EuclideanSpace ℂ (Fin n)))
 
-noncomputable instance {n : ℕ} : InnerProductSpace ℂ (FiniteHilbertSpace n) := inferInstanceAs
-  (InnerProductSpace ℂ (EuclideanSpace ℂ (Fin n)))
+-- noncomputable instance {n : ℕ} : Module ℂ (FiniteHilbertSpace n) := inferInstanceAs
+--   (Module ℂ (EuclideanSpace ℂ (Fin n)))
 
-noncomputable instance {n : ℕ} : CompleteSpace (FiniteHilbertSpace n) := inferInstanceAs
-  (CompleteSpace (EuclideanSpace ℂ (Fin n)))
+-- noncomputable instance {n : ℕ} : NormedAddCommGroup (FiniteHilbertSpace n) := inferInstanceAs
+--   (NormedAddCommGroup (EuclideanSpace ℂ (Fin n)))
+
+-- noncomputable instance {n : ℕ} : InnerProductSpace ℂ (FiniteHilbertSpace n) := inferInstanceAs
+--   (InnerProductSpace ℂ (EuclideanSpace ℂ (Fin n)))
+
+-- noncomputable instance {n : ℕ} : CompleteSpace (FiniteHilbertSpace n) := inferInstanceAs
+--   (CompleteSpace (EuclideanSpace ℂ (Fin n)))
+
+
+namespace FiniteHilbertSpace
+
+variable {d : Type*} [Fintype d] [DecidableEq d]
+
+/-- The equivalence between `FiniteHilbertSpace d` and `EuclideanSpace ℂ d`
+  given by `val`. -/
+def equivEuclidean : FiniteHilbertSpace d ≃ EuclideanSpace ℂ d where
+  toFun := val
+  invFun := mk
+  left_inv _ := rfl
+  right_inv _ := rfl
+
+/-!
+
+## The vector space structure on `FiniteHilbertSpace d`
+
+The vector space structure is transferred from `EuclideanSpace ℂ d`
+along the equivalence `equivEuclidean`.
+
+-/
+
+noncomputable instance : AddCommGroup (FiniteHilbertSpace d) := equivEuclidean.addCommGroup
+
+noncomputable instance : Module ℂ (FiniteHilbertSpace d) := equivEuclidean.module ℂ
+
+@[simp]
+lemma val_add (ψ φ : FiniteHilbertSpace d) : (ψ + φ).val = ψ.val + φ.val := rfl
+
+@[simp]
+lemma val_smul (c : ℂ) (ψ : FiniteHilbertSpace d) : (c • ψ).val = c • ψ.val := rfl
+
+@[simp]
+lemma val_zero : (0 : FiniteHilbertSpace d).val = 0 := rfl
+
+/-- The equivalence between `FiniteHilbertSpace d` and `EuclideanSpace ℂ d`
+  as a `ℂ`-linear equivalence, upgrading `equivEuclidean`. -/
+noncomputable def linearEquivEuclidean : FiniteHilbertSpace d ≃ₗ[ℂ] EuclideanSpace ℂ d :=
+  { equivEuclidean with
+    map_add' := fun _ _ => rfl
+    map_smul' := fun _ _ => rfl }
+
+/-!
+
+## The Hilbert space structure on `FiniteHilbertSpace d`
+
+The norm and inner product are induced from `EuclideanSpace ℂ d` along
+`linearEquivEuclidean`, making `FiniteHilbertSpace d` a finite dimensional
+(and hence complete) inner product space, that is, a Hilbert space.
+
+-/
+
+noncomputable instance : NormedAddCommGroup (FiniteHilbertSpace d) :=
+  NormedAddCommGroup.induced _ _ linearEquivEuclidean.toLinearMap linearEquivEuclidean.injective
+
+@[simp]
+lemma norm_eq_val (ψ : FiniteHilbertSpace d) : ‖ψ‖ = ‖ψ.val‖ := rfl
+
+noncomputable instance : InnerProductSpace ℂ (FiniteHilbertSpace d) :=
+  InnerProductSpace.induced linearEquivEuclidean.toLinearMap
+
+@[simp]
+lemma inner_eq_val (ψ φ : FiniteHilbertSpace d) : inner ℂ ψ φ = inner ℂ ψ.val φ.val := rfl
+
+instance : FiniteDimensional ℂ (FiniteHilbertSpace d) :=
+  Module.Finite.equiv linearEquivEuclidean.symm
+
+instance : CompleteSpace (FiniteHilbertSpace d) := FiniteDimensional.complete ℂ _
+
+/-- The equivalence between `FiniteHilbertSpace d` and `EuclideanSpace ℂ d`
+  as a linear isometry equivalence, upgrading `linearEquivEuclidean`. -/
+noncomputable def isometryEquivEuclidean : FiniteHilbertSpace d ≃ₗᵢ[ℂ] EuclideanSpace ℂ d where
+  toLinearEquiv := linearEquivEuclidean
+  norm_map' _ := rfl
+
+/-!
+
+## The standard orthonormal basis of `FiniteHilbertSpace d`
+
+-/
+
+/-- The standard orthonormal basis of `FiniteHilbertSpace d`, indexed by `d`.
+  Physically, this is the basis of states localized at the points of `d`. -/
+noncomputable def basisFun (d : Type*) [Fintype d] [DecidableEq d] :
+    OrthonormalBasis d ℂ (FiniteHilbertSpace d) :=
+  (EuclideanSpace.basisFun d ℂ).map isometryEquivEuclidean.symm
+
+lemma basisFun_apply (i : d) : basisFun d i = ⟨EuclideanSpace.single i 1⟩ := by
+  rw [basisFun, OrthonormalBasis.map_apply, EuclideanSpace.basisFun_apply]; rfl
+
+end FiniteHilbertSpace
 
 end QuantumMechanics
