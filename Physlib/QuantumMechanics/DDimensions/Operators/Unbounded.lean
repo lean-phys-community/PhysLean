5114b203d4468a4326ef5a0298f33e663e633007
gloges <gerg1995@gmail.com>
2026-06-19 11:47:53 +0900

    adjoint zero fix

--- Physlib/QuantumMechanics/DDimensions/Operators/Unbounded.lean
+++ Physlib/QuantumMechanics/DDimensions/Operators/Unbounded.lean
@@ -303,7 +303,7 @@ lemma adjoint_of_zero [CompleteSpace H] (h_zero : ⇑U = 0) : U† = 0 := by
     · exact adjoint_apply_of_not_dense h x
 
 @[simp]
-lemma adjoint_zero [CompleteSpace H] : (0 : H →ₗ.[ℂ] H)† = 0 := adjoint_of_zero rfl
+lemma adjoint_zero [CompleteSpace H] : (0 : H →ₗ.[ℂ] H')† = 0 := adjoint_of_zero rfl
 
 @[simp]
 lemma adjoint_smul [CompleteSpace H] (U : H →ₗ.[ℂ] H') {c : ℂ} (hc : c ≠ 0) :
