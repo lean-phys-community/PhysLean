cb9edc4b9e07ac79e21b9b823f2579d6bc835590
Nathaneal <nateabr@gmail.com>
2026-06-22 12:12:59 +0400

    docs: order authors alphabetically by last name

--- Physlib/ClassicalMechanics/HarmonicOscillator/Solution.lean
+++ Physlib/ClassicalMechanics/HarmonicOscillator/Solution.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith, Lode Vermeulen, Nathaneal Sajan
+Authors: Nathaneal Sajan, Joseph Tooby-Smith, Lode Vermeulen
 -/
 module
 
