a60f837802234ca4a78adcddc06679091d35b8a3
gloges <gerg1995@gmail.com>
2026-06-24 00:49:00 +0900

    composition

--- Physlib/QuantumMechanics/DDimensions/Operators/SpectralTheory/SpectralMeasure.lean
+++ Physlib/QuantumMechanics/DDimensions/Operators/SpectralTheory/SpectralMeasure.lean
@@ -17,6 +17,9 @@ public import Mathlib.MeasureTheory.VectorMeasure.Basic
 
 ## iii. Table of contents
 
+- A. Definition
+- B. Composition
+
 ## iv. References
 
 -/
@@ -25,8 +28,13 @@ public import Mathlib.MeasureTheory.VectorMeasure.Basic
 
 noncomputable section
 
+open ContinuousLinearMap
 open MeasureTheory
 open Set
+
+/-!
+## A. Definition
+-/
 
 /-- A _spectral measure_ on a measurable space `α` is a σ-additive function `Set α → H →L[ℂ] H`
   such that each set is mapped to a star projection on `H`, the empty set and non-measurable sets
@@ -44,6 +52,8 @@ variable {α : Type*} [MeasurableSpace α]
 variable {H : Type*} [NormedAddCommGroup H] [InnerProductSpace ℂ H] [CompleteSpace H]
 variable (μS : SpectralMeasure α H)
 
+instance : IsAddTorsionFree (H →L[ℂ] H) := sorry
+
 attribute [coe] toVectorMeasure
 
 instance instCoeVectorMeasure : Coe (SpectralMeasure α H) (VectorMeasure α (H →L[ℂ] H)) :=
@@ -56,6 +66,39 @@ lemma isStarProjection (A : Set α) : IsStarProjection (μS A) := μS.isStarProj
 
 @[simp]
 lemma univ : μS univ = 1 := μS.univ'
+
+/-!
+## B. Composition
+-/
+
+@[simp]
+lemma comp_self (A : Set α) : μS A ∘L μS A = μS A := (μS.isStarProjection A).isIdempotentElem
+
+lemma comp_of_disjoint
+    {A B : Set α} (h : Disjoint A B) (hA : MeasurableSet A) (hB : MeasurableSet B) :
+    μS A ∘L μS B = 0 := by
+  suffices μS A ∘L μS (A ∪ B) = μS A by simp_all [μS.of_union]
+  refine (IsStarProjection.sub_iff_mul_eq_left ?_ ?_).mp ?_
+  · exact μS.isStarProjection A
+  · exact μS.isStarProjection (A ∪ B)
+  · rw [μS.of_union h hA hB, add_sub_cancel_left]
+    exact μS.isStarProjection B
+
+lemma comp_eq_of_inter {A B : Set α} (hA : MeasurableSet A) (hB : MeasurableSet B) :
+    μS A ∘L μS B = μS (A ∩ B) := by
+  nth_rw 1 [← inter_union_diff B A, ← inter_union_diff A B]
+  simp only [μS.of_union, hA.inter hB, hB.inter hA, hA.diff hB, hB.diff hA,
+    disjoint_sdiff_inter.symm, add_comp, comp_add]
+  rw [inter_comm B A, μS.comp_of_disjoint disjoint_sdiff_inter (hA.diff hB) (hA.inter hB)]
+  rw [inter_comm A B, μS.comp_of_disjoint disjoint_sdiff_inter.symm (hB.inter hA) (hB.diff hA)]
+  simp [μS.comp_of_disjoint disjoint_sdiff_sdiff (hA.diff hB) (hB.diff hA)]
+
+lemma commute (A B : Set α) : Commute (μS A) (μS B) := by
+  by_cases hAB : MeasurableSet A ∧ MeasurableSet B
+  · simp [commute_iff_eq, mul_def, comp_eq_of_inter, hAB, inter_comm]
+  · rcases not_and_or.mp hAB with hA | hB
+    · simp [hA]
+    · simp [hB]
 
 end SpectralMeasure
 
