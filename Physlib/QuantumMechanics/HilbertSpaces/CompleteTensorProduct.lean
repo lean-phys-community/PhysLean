6371dc5619b725fa284c29676c852a267abce7c1
gloges <gerg1995@gmail.com>
2026-08-03 09:03:00 +0900

    nontrivial

--- Physlib/QuantumMechanics/HilbertSpaces/CompleteTensorProduct.lean
+++ Physlib/QuantumMechanics/HilbertSpaces/CompleteTensorProduct.lean
@@ -21,6 +21,10 @@ noncomputable section
 open UniformSpace
 open scoped InnerProductSpace TensorProduct
 
+/-!
+## A. Definition
+-/
+
 /-- The _completion_ of the tensor product of two inner product spaces `E` and `F` over `𝕜`.
   By construction this produces a Hilbert space. The localized notations are `E ⊗ₕ F`
   and `E ⊗ₕ[𝕜] F`, accessed by `open scoped CompleteTensorProduct`. -/
@@ -36,4 +40,29 @@ scoped[CompleteTensorProduct] infixl:100 " ⊗ₕ " => CompleteTensorProduct _
 scoped[CompleteTensorProduct]
 notation:100 E:100 " ⊗ₕ[" 𝕜 "] " F:101 => CompleteTensorProduct 𝕜 E F
 
+namespace CompleteTensorProduct
+
+/-!
+## B. Nontrivial
+-/
+
+section Nontrivial
+
+variable (𝕜 : Type*) [RCLike 𝕜]
+variable (E : Type*) [NormedAddCommGroup E] [InnerProductSpace 𝕜 E] [Nontrivial E]
+variable (F : Type*) [NormedAddCommGroup F] [InnerProductSpace 𝕜 F] [Nontrivial F]
+
+instance _root_.TensorProduct.instNontrivial : Nontrivial (E ⊗[𝕜] F) where
+  exists_pair_ne := by
+    obtain ⟨x, hx⟩ := exists_ne (0 : E)
+    obtain ⟨y, hy⟩ := exists_ne (0 : F)
+    exact ⟨x ⊗ₜ y, 0, norm_pos_iff.mp (by simp [hx, hy])⟩
+
+instance instNontrivial : Nontrivial (E ⊗ₕ[𝕜] F) where
+  exists_pair_ne := by
+    obtain ⟨x, hx⟩ := exists_ne (0 : E ⊗[𝕜] F)
+    exact ⟨Completion.coe' x, 0, fun h ↦ hx <| Completion.coe_eq_zero_iff.mp h⟩
+
+end Nontrivial
+end CompleteTensorProduct
 end
